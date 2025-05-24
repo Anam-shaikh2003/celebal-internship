b716c52c53784e84e25791b8ef6f4a3bae7f0da5
Anam shaikh <anamshaikh62875@gmail.com>
2025-05-24 22:59:23 +0530

    Add files via upload

---
+++ Reactform/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css"
+
+import Reactform from "./Reactform.jsx";
+  function App(){
+    return (
+     <>
+     {/* <Form2/> */}
+     {/* <Signup/> */}
+    <Reactform/>
+     </>
+      
+
+  )
+
+  }
+  
+
+export default App
